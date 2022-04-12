ac83d6874d3f89ec6fa4465cca3b0961429cf770
adcrosman <adcrosman0@gmail.com>
2022-04-12 10:56:10 -0700

    updating 7.6 again and 8.3.3

--- Unit8/SQLFiles Tier 2/SQLTasks Tier 2.sql
+++ Unit8/SQLFiles Tier 2/SQLTasks Tier 2.sql
@@ -61,28 +61,28 @@ more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 
 SELECT name, monthlymaintenance,
-    CASE WHEN monthlymaintenance > 100 THEN `expensive`
-    ELSE `cheap` END
+    CASE WHEN monthlymaintenance > 100 THEN "expensive"
+    ELSE "cheap" END
     AS cost
-FROM `Facilities`;
+FROM Facilities;
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
 
-SELECT firstname, surname FROM `Members` WHERE joindate = (SELECT MAX(joindate) AS newest FROM `Members`)
+SELECT firstname, surname FROM Members WHERE joindate = (SELECT MAX(joindate) AS newest FROM Members)
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
 
-SELECT DISTINCT f.name, CONCAT(m.firstname, ` `, m.surname) AS fullname
-FROM `Bookings` AS b
-LEFT JOIN `Facilities` AS f
+SELECT DISTINCT f.name, m.firstname || m.surname AS fullname
+FROM Bookings AS b
+LEFT JOIN Facilities AS f
 USING (facid)
-LEFT JOIN `Members` AS m
+LEFT JOIN Members AS m
 USING (memid)
-WHERE facid = 0 OR facid = 1
+WHERE memid <> 0 AND (facid = 0 OR facid = 1)
 ORDER BY fullname;
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
@@ -92,26 +92,26 @@ the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 
-SELECT f.name, CONCAT(m.firstname, ` `, m.surname) AS fullname,
+SELECT f.name, m.firstname || m.surname AS fullname,
     CASE WHEN memid = 0 THEN f.guestcost
-    ELSE f.memcost END AS cost
-FROM `Bookings` AS b
-LEFT JOIN `Facilities` AS f
+    ELSE f.membercost END AS cost
+FROM Bookings AS b
+LEFT JOIN Facilities AS f
 USING (facid)
-LEFT JOIN `Members` AS m
+LEFT JOIN Members AS m
 USING (memid)
-WHERE cost > 30 AND starttime LIKE `2012-09-14%`
+WHERE cost > 30 AND starttime LIKE "2012-09-14%"
 ORDER BY cost DESC;
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 
-SELECT f.name, CONCAT(m.firstname, ` `, m.surname) AS fullname,
+SELECT f.name, m.firstname || m.surname AS fullname
     CASE WHEN memid = 0 THEN f.guestcost
-    ELSE f.memcost END AS cost
-FROM (SELECT facid, memid, starttime FROM Bookings WHERE starttime LIKE `2012-09-14%`) AS subset
-LEFT JOIN `Facilities` AS f
+    ELSE f.membercost END AS cost
+FROM (SELECT facid, memid, starttime FROM Bookings WHERE starttime LIKE "2012-09-14%") AS subset
+LEFT JOIN Facilities AS f
 USING (facid)
-LEFT JOIN `Members` AS m
+LEFT JOIN Members AS m
 USING (memid)
 WHERE cost > 30
 ORDER BY cost DESC;
@@ -129,12 +129,12 @@ that there's a different cost for guests and members! */
 WITH cte AS(
     SELECT name,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid <> 0) * f.membercost AS memrev,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 0) * f.guestcost AS guestrev
-    FROM FACILITIES AS f)
+    FROM Facilities AS f)
 SELECT name, (memrev + guestrev) AS totalrev
 FROM cte
 WHERE totalrev < 1000
@@ -143,8 +143,8 @@ ORDER BY totalrev
 /* Q11: Produce a report of members and who recommended them in alphabetic surname,firstname order */
 
 SELECT m1.surname AS surname, m1.firstname AS firstname, m2.surname AS recsurname, m2.firstname AS recfirstname
-FROM MEMBERS AS m1
-INNER JOIN MEMBERS AS m2
+FROM Members AS m1
+INNER JOIN Members AS m2
 ON m1.recommendedby = m2.memid
 ORDER BY surname, firstname
 
@@ -152,128 +152,128 @@ ORDER BY surname, firstname
 
 SELECT name,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 1) AS oneuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 2) AS twouse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 3) AS threeuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 4) AS fouruse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 5) AS fiveuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 6) AS sixuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 7) AS sevenuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 8) AS eightuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 9) AS nineuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 10) AS tenuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 11) AS elevenuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 12) AS twelveuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 13) AS thirteenuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 14) AS fourteenuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 15) AS fifteenuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 16) AS sixteenuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 17) AS seventeenuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 18) AS eighteenuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 19) AS nineteenuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 20) AS twentyuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 21) AS twentyoneuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 22) AS twentyetwouse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 23) AS twentythreeuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 24) AS twentyfouruse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 25) AS twentyfiveuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 26) AS twentysixuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 27) AS twentysevenuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 28) AS twentyeightuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 29) AS twentynineuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 30) AS thirtyuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 31) AS thirtyoneuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 32) AS thirtyetwouse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 33) AS thirtythreeuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 34) AS thirtyfouruse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 35) AS thirtyfiveuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 36) AS thirtysixuse,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
+        FROM Bookings AS b
         WHERE f.facid = b.facid AND memid = 37) AS thirtysevenuse
-FROM FACILITIES AS f
+FROM Facilities AS f
 
 /* Q13: Find the facilities usage by month, but not guests */
 
 SELECT name,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
-        WHERE f.facid = b.facid AND starttime LIKE `2012-07%` AND memid <> 0) AS July,
+        FROM Bookings AS b
+        WHERE f.facid = b.facid AND starttime LIKE "2012-07%" AND memid <> 0) AS July,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
-        WHERE f.facid = b.facid AND starttime LIKE `2012-08%` AND memid <> 0) AS August,
+        FROM Bookings AS b
+        WHERE f.facid = b.facid AND starttime LIKE "2012-08%" AND memid <> 0) AS August,
     (SELECT COUNT(facid)
-        FROM BOOKINGS AS b
-        WHERE f.facid = b.facid AND starttime LIKE `2012-09%` AND memid <> 0) AS September
-FROM FACILITIES AS f
+        FROM Bookings AS b
+        WHERE f.facid = b.facid AND starttime LIKE "2012-09%" AND memid <> 0) AS September
+FROM Facilities AS f
